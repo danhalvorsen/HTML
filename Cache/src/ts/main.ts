d6d9a7d228bbd67efb2611da525482e9aae7e0a5
Dan Halvorsen <dehalvorsen@gmail.com>
2022-03-07 00:09:16 +0100

    Working with integration testing

--- Cache/src/ts/main.ts
+++ Cache/src/ts/main.ts
@@ -1,18 +1,27 @@
-import { openDB } from "idb";
 import * as _ from "lodash";
 import { Database } from "./Database";
 
-//Execute the program when the DOM is read and loaded
-window.addEventListener("load", (event) => {
-  main();
+document.addEventListener("DOMContentLoaded", async function (event) {
+  console.log("calling main()");
+  await main();
 });
 
-const storeName: string = "my-test-pair";
-
 export async function main() {
-  var database = new Database();
+  document
+    .getElementById("save")
+    .addEventListener("click", async (event: MouseEvent) => {
+      console.log("save pressed");
 
-  await database.CreateDatabase();
-  await database.Add("key1", "Dan");
+      //var key = document.getElementById("key").innerText;
+      var key = (<HTMLInputElement>document.getElementById("key")).value;
 
+      if (key.length > 5) {
+        console.log("data preset");
+      }
+      var value = (<HTMLInputElement>document.getElementById("value")).value;
+      console.log(`Maybe some data:`, key.toString() + " " + value.toString());
+      var database = new Database();
+      await database.CreateDatabase();
+      await database.Add(key, value);
+    });
 }
