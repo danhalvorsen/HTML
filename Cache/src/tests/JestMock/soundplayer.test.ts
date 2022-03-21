468ec0bb9a39b94bbc097a3967cb5dd002d360fb
Dan Halvorsen <dehalvorsen@gmail.com>
2022-03-21 16:14:12 +0100

    ts-jest

---
+++ Cache/src/tests/JestMock/soundplayer.test.ts
@@ -0,0 +1,29 @@
+import { jest } from '@jest/globals';
+import { mocked } from 'ts-jest/utils';
+import { SoundPlayer } from '../../ts/soundplayer';
+
+jest.mock('./sound-player', () => {
+  return {
+    SoundPlayer: jest.fn().mockImplementation(() => {
+      return {
+        playSoundFile: () => {},
+      };
+    })
+  };
+});
+
+describe('SoundPlayerConsumer', () => {
+  const MockedSoundPlayer = mocked(SoundPlayer, true);
+
+  beforeEach(() => {
+   // Clears the record of calls to the mock constructor function and its methods
+   MockedSoundPlayer.mockClear();
+
+  });
+
+  it('We can check if the consumer called the class constructor', () => {
+    const soundPlayerConsumer = new SoundPlayerConsumer();
+    expect(MockedSoundPlayer).toHaveBeenCalledTimes(1);
+  });
+
+}
