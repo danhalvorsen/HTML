32fb5e902b4268d5be8639794ee519ac615bb0c9
Dan Edvard Halvorsen <DEHA@equinor.com>
2022-04-25 15:37:10 +0200

    typed object in dexie table, so that map2Class() will work as expected

--- my-pwa-ts/src/repositories/base/interfaces/ChecklistDetailsRepository.ts
+++ my-pwa-ts/src/repositories/base/interfaces/ChecklistDetailsRepository.ts
@@ -1,12 +1,18 @@
+import { ChecklistDetailsIndexes } from "../../../database/Index";
 import { db } from "../../../database/mcAppDatabase";
 import { IChecklistDetails } from "../../../mockRoutes/api.types";
 import { BaseRepository } from "./Repository";
 
-export class ChecklistDetailsRepository extends BaseRepository<IChecklistDetails> {
+export class ChecklistDetailsRepository {
+// extends BaseRepository<IChecklistDetails> {
     constructor() {
-      var table = db.tables.find((t) => t.name === "checklistDetails");
-      console.log(`Database:${db.name} is: ${db.isOpen() ? "open" : "closed"}`);
-      console.log(`ChecklistDetailsRepository giving dixie table:${table} to baseRepository`);
-      super(table);
+      
     }
+
+    async getById(id : number) : IChecklistDetails {
+        var result = await db.checklistDetails.where('id').equals(id).first();
+        
+
+    }
+
   }
